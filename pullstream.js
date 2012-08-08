e306ab3f9bb676281d6db7482bf887c0be4f60b6
Joe Ferner <joe@fernsroth.com>
2012-08-08 14:10:21 -0400

    working first tesT

--- pullstream.js
+++ pullstream.js
@@ -4,11 +4,17 @@ module.exports = PullStream;
 
 var inherits = require("util").inherits;
 var Stream = require('stream').Stream;
+var over = require('over');
+var events = require("events");
 
 function PullStream() {
   Stream.apply(this);
   this.readable = false;
   this.writable = true;
+  this._buffer = new Buffer(10 * 1024 * 1024);
+  this._bufferWriteIdx = 0;
+  this._bufferReadIdx = 0;
+  this._emitter = new events.EventEmitter();
 }
 inherits(PullStream, Stream);
 
@@ -21,9 +27,82 @@ PullStream.prototype.end = function (data) {
 };
 
 PullStream.prototype.process = function (data, end) {
+  if (data) {
+    data.copy(this._buffer, this._bufferWriteIdx);
+    this._bufferWriteIdx += data.length;
+    this._emitter.emit('newdata');
+  }
   if (end) {
-    this.emit('end');
+    this._end = true;
+    this._emitter.emit('end');
   }
 };
 
+PullStream.prototype.pull = over([
+  [over.number, over.func, function (len, callback) {
+    var self = this;
+    self._emitter.on('newdata', dataOrEnd.bind(self, 'newdata'));
+    self._emitter.on('end', dataOrEnd.bind(self, 'end'));
 
+    function dataOrEnd(evt) {
+      if (self._bufferWriteIdx - self._bufferReadIdx > len) {
+        var result = this._buffer.slice(self._bufferReadIdx, self._bufferReadIdx + len);
+        self._bufferReadIdx += len;
+        self._emitter.removeAllListeners();
+        callback(null, result);
+        if (evt === 'end') {
+          self.emit('end');
+        }
+      }
+    }
+  }],
+  [over.func, function (callback) {
+    var self = this;
+    self._emitter.on('end', function () {
+      var len = self._bufferWriteIdx - self._bufferReadIdx;
+      var result = self._buffer.slice(self._bufferReadIdx, self._bufferReadIdx + len);
+      self._bufferReadIdx += len;
+      self._emitter.removeAllListeners();
+      callback(null, result);
+      self.emit('end');
+    });
+  }]
+]);
+
+PullStream.prototype.pipe = over([
+  [over.number, over.object, function (len, destStream) {
+    this._pipe(len, destStream);
+  }],
+  [over.object, function (destStream) {
+    throw new Error("Not implemented");
+    //this._pull(null, destStream);
+  }]
+]);
+
+PullStream.prototype._pipe = function (len, destStream) {
+  var self = this;
+  this._emitter.on('newdata', dataOrEnd.bind(this, 'newdata'));
+  this._emitter.on('end', dataOrEnd.bind(this, 'end'));
+  process.nextTick(function () {
+    dataOrEnd('newdata');
+  });
+
+  function dataOrEnd(evt) {
+    var lenToSend = Math.min(self._bufferWriteIdx - self._bufferReadIdx, len);
+    if (lenToSend > 0) {
+      var result = self._buffer.slice(self._bufferReadIdx, self._bufferReadIdx + lenToSend);
+      self._bufferReadIdx += lenToSend;
+      var newLen = len - lenToSend;
+      destStream.write(result);
+      self._emitter.removeAllListeners();
+      if (newLen === 0) {
+        destStream.end();
+      } else {
+        self._pipe(newLen, destStream);
+      }
+      if (evt === 'end') {
+        self.emit('end');
+      }
+    }
+  }
+};
--- test/pullStreamTest.js
+++ test/pullStreamTest.js
@@ -5,8 +5,11 @@ var streamBuffers = require("stream-buffers");
 var PullStream = require('../');
 
 test("source sending 1-byte at a time", function (t) {
+  t.plan(3);
+
   var ps = new PullStream();
   ps.on('end', function () {
+    sourceStream.destroy();
     t.end();
   });
 
@@ -14,7 +17,32 @@ test("source sending 1-byte at a time", function (t) {
     frequency: 0,
     chunkSize: 1
   });
+  sourceStream.put("Hello World!");
 
   sourceStream.pipe(ps);
+
+  ps.pull('Hello'.length, function (err, data) {
+    if (err) {
+      t.fail(err);
+    }
+    t.equal('Hello', data.toString());
+
+    var writableStream = new streamBuffers.WritableStreamBuffer({
+      initialSize: 100
+    });
+    writableStream.on('close', function () {
+      var str = writableStream.getContentsAsString('utf8');
+      t.equal(' World', str);
+
+      ps.pull(function (err, data) {
+        if (err) {
+          t.fail(err);
+        }
+        t.equal('!', data.toString());
+      });
+    });
+
+    ps.pipe(' World'.length, writableStream);
+  });
 });
 
