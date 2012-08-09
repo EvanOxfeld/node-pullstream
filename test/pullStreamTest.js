486ca18b3de378cf386379ea3b8f17468190afb8
Joe Ferner <joe@fernsroth.com>
2012-08-09 16:11:56 -0400

    conver to nodeunit

--- test/pullStreamTest.js
+++ test/pullStreamTest.js
@@ -1,313 +1,294 @@
 'use strict';
 
-var test = require('tap').test;
+var nodeunit = require('nodeunit');
 var fs = require("fs");
 var path = require("path");
 var streamBuffers = require("stream-buffers");
 var PullStream = require('../');
 
-test("source sending 1-byte at a time", function (t) {
-  t.plan(3);
-
-  var ps = new PullStream();
-  ps.on('end', function () {
-    sourceStream.destroy();
-    t.end();
-  });
-
-  var sourceStream = new streamBuffers.ReadableStreamBuffer({
-    frequency: 0,
-    chunkSize: 1
-  });
-  sourceStream.put("Hello World!");
-
-  sourceStream.pipe(ps);
-
-  ps.pull('Hello'.length, function (err, data) {
-    if (err) {
-      return t.fail(err);
-    }
-    t.equal('Hello', data.toString());
-
-    var writableStream = new streamBuffers.WritableStreamBuffer({
-      initialSize: 100
-    });
-    writableStream.on('close', function () {
-      var str = writableStream.getContentsAsString('utf8');
-      t.equal(' World', str);
-
-      ps.pull(function (err, data) {
-        if (err) {
-          return t.fail(err);
-        }
-        t.equal('!', data.toString());
-      });
+module.exports = {
+  "source sending 1-byte at a time": function (t) {
+    var ps = new PullStream();
+    ps.on('end', function () {
+      sourceStream.destroy();
+      t.done();
     });
 
-    ps.pipe(' World'.length, writableStream);
-  });
-});
-
-test("source sending all data at once", function (t) {
-  t.plan(3);
-
-  var ps = new PullStream();
-  ps.on('end', function () {
-    sourceStream.destroy();
-    t.end();
-  });
-
-  var sourceStream = new streamBuffers.ReadableStreamBuffer({
-    frequency: 0,
-    chunkSize: 1000
-  });
-  sourceStream.put("Hello World!");
-
-  sourceStream.pipe(ps);
-
-  ps.pull('Hello'.length, function (err, data) {
-    if (err) {
-      return t.fail(err);
-    }
-    t.equal('Hello', data.toString());
-
-    var writableStream = new streamBuffers.WritableStreamBuffer({
-      initialSize: 100
+    var sourceStream = new streamBuffers.ReadableStreamBuffer({
+      frequency: 0,
+      chunkSize: 1
     });
-    writableStream.on('close', function () {
-      var str = writableStream.getContentsAsString('utf8');
-      t.equal(' World', str);
+    sourceStream.put("Hello World!");
 
-      ps.pull(function (err, data) {
-        if (err) {
-          t.fail(err);
-        }
-        t.equal('!', data.toString());
-      });
-    });
+    sourceStream.pipe(ps);
 
-    ps.pipe(' World'.length, writableStream);
-  });
-});
-
-test("two length pulls", function (t) {
-  t.plan(2);
-
-  var ps = new PullStream();
-  ps.on('end', function () {
-    sourceStream.destroy();
-    t.end();
-  });
-
-  var sourceStream = new streamBuffers.ReadableStreamBuffer({
-    frequency: 0,
-    chunkSize: 1000
-  });
-  sourceStream.put("Hello World!");
-
-  sourceStream.pipe(ps);
-
-  ps.pull('Hello'.length, function (err, data) {
-    if (err) {
-      return t.fail(err);
-    }
-    t.equal('Hello', data.toString());
-
-    ps.pull(' World!'.length, function (err, data) {
-      if (err) {
-        return t.fail(err);
-      }
-      t.equal(' World!', data.toString());
-    });
-  });
-});
-
-test("read from file", function (t) {
-  t.plan(2);
-
-  var ps = new PullStream();
-  ps.on('end', function () {
-    t.end();
-  });
-
-  var sourceStream = fs.createReadStream(path.join(__dirname, 'testFile.txt'));
-
-  sourceStream.pipe(ps);
-
-  ps.pull('Hello'.length, function (err, data) {
-    if (err) {
-      return t.fail(err);
-    }
-    t.equal('Hello', data.toString());
-
-    ps.pull(' World!'.length, function (err, data) {
-      if (err) {
-        return t.fail(err);
-      }
-      t.equal(' World!', data.toString());
-    });
-  });
-});
-
-test("read from file pipe pause/resume", function (t) {
-  t.plan(3);
-
-  var ps = new PullStream();
-
-  var sourceStream = fs.createReadStream(path.join(__dirname, 'testFile.txt'));
-
-  sourceStream.pipe(ps);
-
-  ps.pause();
-  ps.pull('Hello'.length, function (err, data) {
-    if (err) {
-      return t.fail(err);
-    }
-    t.equal('Hello', data.toString());
-
-    ps.pull(' World!'.length, function (err, data) {
-      if (err) {
-        return t.fail(err);
-      }
-      t.equal(' World!', data.toString());
-
-      ps.pull(5, function (err, data) {
-        t.ok(err, 'end of file should happen');
-        return t.end();
-      });
-    });
-  });
-
-  process.nextTick(function () {
-    ps.resume();
-  });
-});
-
-test("pause/resume", function (t) {
-  t.plan(2);
-
-  var ps = new PullStream();
-  ps.on('end', function () {
-    sourceStream.destroy();
-    t.end();
-  });
-
-  var sourceStream = new streamBuffers.ReadableStreamBuffer({
-    frequency: 0,
-    chunkSize: 1000
-  });
-  sourceStream.put("Hello World!");
-
-  sourceStream.pipe(ps);
-
-  ps.pause();
-  process.nextTick(function () {
-    ps.resume();
     ps.pull('Hello'.length, function (err, data) {
       if (err) {
-        return t.fail(err);
+        return t.done(err);
+      }
+      t.equal('Hello', data.toString());
+
+      var writableStream = new streamBuffers.WritableStreamBuffer({
+        initialSize: 100
+      });
+      writableStream.on('close', function () {
+        var str = writableStream.getContentsAsString('utf8');
+        t.equal(' World', str);
+
+        ps.pull(function (err, data) {
+          if (err) {
+            return t.done(err);
+          }
+          t.equal('!', data.toString());
+        });
+      });
+
+      ps.pipe(' World'.length, writableStream);
+    });
+  },
+
+  "source sending all data at once": function (t) {
+    var ps = new PullStream();
+    ps.on('end', function () {
+      sourceStream.destroy();
+      return t.done();
+    });
+
+    var sourceStream = new streamBuffers.ReadableStreamBuffer({
+      frequency: 0,
+      chunkSize: 1000
+    });
+    sourceStream.put("Hello World!");
+
+    sourceStream.pipe(ps);
+
+    ps.pull('Hello'.length, function (err, data) {
+      if (err) {
+        return t.done(err);
+      }
+      t.equal('Hello', data.toString());
+
+      var writableStream = new streamBuffers.WritableStreamBuffer({
+        initialSize: 100
+      });
+      writableStream.on('close', function () {
+        var str = writableStream.getContentsAsString('utf8');
+        t.equal(' World', str);
+
+        ps.pull(function (err, data) {
+          if (err) {
+            return t.done(err);
+          }
+          t.equal('!', data.toString());
+        });
+      });
+
+      ps.pipe(' World'.length, writableStream);
+    });
+  },
+
+  "two length pulls": function (t) {
+    var ps = new PullStream();
+    ps.on('end', function () {
+      sourceStream.destroy();
+      return t.done();
+    });
+
+    var sourceStream = new streamBuffers.ReadableStreamBuffer({
+      frequency: 0,
+      chunkSize: 1000
+    });
+    sourceStream.put("Hello World!");
+
+    sourceStream.pipe(ps);
+
+    ps.pull('Hello'.length, function (err, data) {
+      if (err) {
+        return t.done(err);
       }
       t.equal('Hello', data.toString());
 
       ps.pull(' World!'.length, function (err, data) {
         if (err) {
-          t.fail(err);
+          return t.done(err);
         }
         t.equal(' World!', data.toString());
       });
     });
-  });
-});
+  },
 
-test("read past end of stream", function (t) {
-  t.plan(1);
-
-  var ps = new PullStream();
-  ps.on('end', function () {
-    sourceStream.destroy();
-    t.end();
-  });
-
-  var sourceStream = new streamBuffers.ReadableStreamBuffer({
-    frequency: 0,
-    chunkSize: 1000
-  });
-  sourceStream.put("Hello World!");
-
-  sourceStream.pipe(ps);
-
-  ps.pull('Hello World!'.length, function (err, data) {
-    if (err) {
-      return t.fail(err);
-    }
-    t.equal('Hello World!', data.toString());
-
-    ps.pull(1, function (err, data) {
-      if (err) {
-        return; // ok
-      }
-      t.fail('should get an error');
+  "read from file": function (t) {
+    var ps = new PullStream();
+    ps.on('end', function () {
+      return t.done();
     });
-  });
-});
 
-test("pause/resume using writes", function (t) {
-  t.plan(2);
+    var sourceStream = fs.createReadStream(path.join(__dirname, 'testFile.txt'));
 
-  var isResumed = false;
-  var ps = new PullStream();
-  ps.pause();
-  ps.pull('Hello World!'.length, function (err, data) {
-    if (err) {
-      return t.fail(err);
-    }
-    t.ok(isResumed, 'Stream is resumed');
-    t.equal('Hello World!', data.toString());
-    t.end();
-  });
-  ps.write(new Buffer('Hello World!', 'utf8'));
-  ps.end();
-  process.nextTick(function () {
-    isResumed = true;
-    ps.resume();
-  });
-});
+    sourceStream.pipe(ps);
 
-test("pause/resume using writes pause after first pull", function (t) {
-  t.plan(4);
-
-  var isResumed = false;
-  var ps = new PullStream();
-  ps.pull('Hello '.length, function (err, data) {
-    if (err) {
-      return t.fail(err);
-    }
-    t.equal('Hello ', data.toString());
-    ps.pause();
-    ps.pull('World!'.length, function (err, data) {
+    ps.pull('Hello'.length, function (err, data) {
       if (err) {
-        return t.fail(err);
+        return t.done(err);
       }
-      t.ok(isResumed, 'isResumed');
-      t.equal('World!', data.toString());
+      t.equal('Hello', data.toString());
 
-      ps.pull(10, function (err, data) {
+      ps.pull(' World!'.length, function (err, data) {
         if (err) {
-          t.ok(err, 'should be an error');
-          return t.end(); // good
+          return t.done(err);
         }
-        return t.fail('should be end of file');
-      });
-
-      process.nextTick(function () {
-        ps.end();
+        t.equal(' World!', data.toString());
       });
     });
+  },
+
+  "read from file pipe pause/resume": function (t) {
+    var ps = new PullStream();
+
+    var sourceStream = fs.createReadStream(path.join(__dirname, 'testFile.txt'));
+
+    sourceStream.pipe(ps);
+
+    ps.pause();
+    ps.pull('Hello'.length, function (err, data) {
+      if (err) {
+        return t.done(err);
+      }
+      t.equal('Hello', data.toString());
+
+      ps.pull(' World!'.length, function (err, data) {
+        if (err) {
+          return t.done(err);
+        }
+        t.equal(' World!', data.toString());
+
+        ps.pull(5, function (err, data) {
+          t.ok(err, 'end of file should happen');
+          return t.done();
+        });
+      });
+    });
+
     process.nextTick(function () {
-      isResumed = true;
       ps.resume();
     });
-  });
-  ps.write(new Buffer('Hello World!', 'utf8'));
-  ps.end();
-});
+  },
+
+  "pause/resume": function (t) {
+    var ps = new PullStream();
+    ps.on('end', function () {
+      sourceStream.destroy();
+      return t.done();
+    });
+
+    var sourceStream = new streamBuffers.ReadableStreamBuffer({
+      frequency: 0,
+      chunkSize: 1000
+    });
+    sourceStream.put("Hello World!");
+
+    sourceStream.pipe(ps);
+
+    ps.pause();
+    process.nextTick(function () {
+      ps.resume();
+      ps.pull('Hello'.length, function (err, data) {
+        if (err) {
+          return t.done(err);
+        }
+        t.equal('Hello', data.toString());
+
+        ps.pull(' World!'.length, function (err, data) {
+          if (err) {
+            return t.done(err);
+          }
+          t.equal(' World!', data.toString());
+        });
+      });
+    });
+  },
+
+  "read past end of stream": function (t) {
+    var ps = new PullStream();
+    ps.on('end', function () {
+      sourceStream.destroy();
+      return t.done();
+    });
+
+    var sourceStream = new streamBuffers.ReadableStreamBuffer({
+      frequency: 0,
+      chunkSize: 1000
+    });
+    sourceStream.put("Hello World!");
+
+    sourceStream.pipe(ps);
+
+    ps.pull('Hello World!'.length, function (err, data) {
+      if (err) {
+        return t.done(err);
+      }
+      t.equal('Hello World!', data.toString());
+
+      ps.pull(1, function (err, data) {
+        if (err) {
+          return; // ok
+        }
+        return t.done('should get an error');
+      });
+    });
+  },
+
+  "pause/resume using writes": function (t) {
+    var isResumed = false;
+    var ps = new PullStream();
+    ps.pause();
+    ps.pull('Hello World!'.length, function (err, data) {
+      if (err) {
+        return t.done(err);
+      }
+      t.ok(isResumed, 'Stream is resumed');
+      t.equal('Hello World!', data.toString());
+      return t.done();
+    });
+    ps.write(new Buffer('Hello World!', 'utf8'));
+    ps.end();
+    setTimeout(function () {
+      isResumed = true;
+      ps.resume();
+    }, 100);
+  },
+
+  "pause/resume using writes pause after first pull": function (t) {
+    var isResumed = false;
+    var ps = new PullStream();
+    ps.pull('Hello '.length, function (err, data) {
+      if (err) {
+        return t.done(err);
+      }
+      t.equal('Hello ', data.toString());
+      ps.pause();
+      ps.pull('World!'.length, function (err, data) {
+        if (err) {
+          return t.done(err);
+        }
+        t.ok(isResumed, 'isResumed');
+        t.equal('World!', data.toString());
+
+        ps.pull(10, function (err, data) {
+          if (err) {
+            t.ok(err, 'should be an error');
+            return t.done(); // good
+          }
+          return t.done('should be end of file');
+        });
+      });
+      setTimeout(function () {
+        isResumed = true;
+        ps.resume();
+      }, 100);
+    });
+    ps.write(new Buffer('Hello World!', 'utf8'));
+    ps.end();
+  }
+};
+
